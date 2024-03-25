8b6f4ada6aa1a35513570cfab5942925cce19ee1
Meghana <latashillc@gmail.com>
2024-03-25 10:36:28 -0400

    Test Github push

--- VolunteerHoursTracker/AddTransactionView.swift
+++ VolunteerHoursTracker/AddTransactionView.swift
@@ -16,7 +16,7 @@ struct AddTransactionView: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello test github")
     }
 }
 
